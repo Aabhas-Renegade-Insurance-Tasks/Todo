2b082352924d77dd6c848acebda857f34278b1ff
Aabhas Dhaubanja <aabhas.dhaubanja@renegadeinsurance.com>
2022-05-03 18:00:29 +0545

    feat: can add todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    const currentLength = todos.length;
+    const newTodo = { id: currentLength + 1, todo };
+    setTodos([...todos, newTodo]);
+  };
+
+  const removeTodo = (id) => {
+    const prevTodo = { ...todos };
+    const newTodos = prevTodo.filter((todo) => id != todo.id);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <div>
-        <label htmlFor="new-todo"></label>
-        <input id="new-todo" type="text" name="new-todo" />
+        <label htmlFor="new-todo">New Todo:</label>
+        <input
+          onChange={(e) => setState(e.target.value)}
+          id="new-todo"
+          type="text"
+          name="new-todo"
+        />
+        <button onClick={() => addTodo(state)}>Add</button>
       </div>
+
+      {todos.map(({ id, todo }) => {
+        return <div key={id}>{todo}</div>;
+      })}
     </div>
   );
 }
