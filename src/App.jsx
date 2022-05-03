bab1c5437fab9d153a7d8e96eb82830bdfafa99b
Aabhas Dhaubanja <aabhas.dhaubanja@renegadeinsurance.com>
2022-05-03 15:54:34 +0545

    feat: added input box for new todos

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <label htmlFor="new-todo"></label>
+        <input id="new-todo" type="text" name="new-todo" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
